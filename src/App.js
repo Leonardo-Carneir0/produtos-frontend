a1c01776269312295bd56060ab5b7e321dc819b7
Leonardo Carneiro <69646391+Leonardo-Carneir0@users.noreply.github.com>
2024-07-16 17:00:11 -0300

    feat: integrar componentes no App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProductList from './components/ProductList';
+import ProductForm from './components/ProductForm';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de Produtos</h1>
+      <ProductForm />
+      <ProductList />
     </div>
   );
-}
+};
 
 export default App;
